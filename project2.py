a7318625ab09cb6a262a786a99fa472eae4e0dcb
Gian Impronta <gian.impronta@gmail.com>
2017-05-09 19:14:25 -0300

    - Removed a magic number - Reduced the number of lines of code for the each method

--- project2.py
+++ project2.py
@@ -5,22 +5,16 @@ import time
 def main():
     level = menu()
     clear()
-    easy = ['''
-    What was the name of R2-D2's sidekick? [__1__].
-    What was the name of Han solo's partner? [__2__].
-    Who was Luke's father? [__3__].
-    Who was Leia's brother? [__4__].''', ['C3PO', 'Chewbacca', 'Darth Vader', 'Luke']]
-    medium = ['''
-    What was the real name of Darth Vader? [__1__].
-    Anakin Skywalker used to compete in [__2__] races when he was a kid.
-    What was the name of the anakin's archi-rival when he was a kid? [__3__].
-    What was the name of planet which Anakin lived? [__4__]
-    ''', ['Anakin Skywalker', 'Pod', 'Sebulba', 'Tatooine']]
-    hard = ['''
-    What was the color of Luke's lightsaber? [__1__].
-    Who defeated Darth Maul? [__2__].
-    What was the name of Anakin' owner when he was still a slave? [__3__]. 
-    Who slavered princess Leia? [__4__].''', ['Blue', 'Obi Wan', 'Watto', 'Jabba the Hutt']]
+    easy = ["What was the name of R2-D2's sidekick? [__1__].\nWhat was the name of Han solo's partner? [__2__].\n"
+            "Who was Luke's father? [__3__].\nWho was Leia's brother? [__4__].",
+            ['C3PO', 'Chewbacca', 'Darth Vader', 'Luke']]
+    medium = [
+        "What was the real name of Darth Vader? [__1__].\nAnakin Skywalker used to compete in [__2__] races when he was a kid.\n"
+        "What was the name of the anakin's archi-rival when he was a kid? [__3__].\nWhat was the name of planet which Anakin lived? [__4__]",
+        ['Anakin Skywalker', 'Pod', 'Sebulba', 'Tatooine']]
+    hard = [
+        "What was the color of Luke's lightsaber? [__1__].\nWho defeated Darth Maul? [__2__].\nWhat was the name of Anakin' owner when he was still a slave? [__3__].\nWho slavered princess Leia? [__4__].",
+        ['Blue', 'Obi Wan', 'Watto', 'Jabba the Hutt']]
     difficulties = [easy, medium, hard]
     questions(difficulties[int(level)])
     congrats()
@@ -32,42 +26,36 @@ def menu():
     :return: level
     :rtype: string
     """
-    print '''
-     _______.___________.    ___      .______         
-    /       |           |   /   \     |   _  \        
-   |   (----`---|  |----`  /  ^  \    |  |_)  |       
-    \   \       |  |      /  /_\  \   |      /        
-.----)   |      |  |     /  _____  \  |  |\  \----.   
-|_______/       |__|    /__/     \__\ | _| `._____|   
-____    __    ____  ___      .______          _______.
-\   \  /  \  /   / /   \     |   _  \        /       |
- \   \/    \/   / /  ^  \    |  |_)  |      |   (----`
-  \            / /  /_\  \   |      /        \   \    
-   \    /\    / /  _____  \  |  |\  \----.----)   |   
-    \__/  \__/ /__/     \__\ | _| `._____|_______/    
-                                                      
-         Welcome to the Star Wars Trivia!    
-    '''
+    logo()
     time.sleep(2)
     while True:
-        print '''   
-###################################
-Please choose your quiz level:
-0 - easy
-1 - medium
-2 - hard
-
-Your choice:'''
-        level = raw_input()
-        if level in ('0', '1', '2'):
-            difficulties = ['easy', 'medium', 'hard']
-            print '''
-###################################
-This the %s level. May the Force be with you!''' % difficulties[int(level)]
+        print "###################################\nPlease choose your quiz level:\n- easy\n- medium\n- hard\n\nYour choice:"
+        level = ['easy', 'medium', 'hard'].index(raw_input().lower())
+        if level in [0, 1, 2]:
+            print "###################################\nThis the %s level. May the Force be with you!" % \
+                  ['easy', 'medium', 'hard'][level]
             break
-
+    time.sleep(1)
     return level
 
+
+def logo():
+    print '''
+         _______.___________.    ___      .______         
+        /       |           |   /   \     |   _  \        
+       |   (----`---|  |----`  /  ^  \    |  |_)  |       
+        \   \       |  |      /  /_\  \   |      /        
+    .----)   |      |  |     /  _____  \  |  |\  \----.   
+    |_______/       |__|    /__/     \__\ | _| `._____|   
+    ____    __    ____  ___      .______          _______.
+    \   \  /  \  /   / /   \     |   _  \        /       |
+     \   \/    \/   / /  ^  \    |  |_)  |      |   (----`
+      \            / /  /_\  \   |      /        \   \    
+       \    /\    / /  _____  \  |  |\  \----.----)   |   
+        \__/  \__/ /__/     \__\ | _| `._____|_______/    
+
+             Welcome to the Star Wars Trivia!    
+        '''
 
 def questions(level):
     """
@@ -75,10 +63,9 @@ def questions(level):
     :param level: level of difficulty chosen by the player
     :type level: list
     """
-    question = level[0]
-    answers = level[1]
+    question, answers = level[0], level[1]
     guessed = 0
-    while guessed < 4:
+    while guessed < len(answers):
         if guessed != 0:
             question = question.replace('[__%d__]' % guessed, answers[guessed - 1])
         while True:
@@ -90,8 +77,6 @@ def questions(level):
             user_answer = raw_input().lower()
             if user_answer == answers[guessed].lower():
                 guessed += 1
-                print 'Nice!'
-                time.sleep(0.5)
                 break
             else:
                 print 'Wrong!\nTry again!'
